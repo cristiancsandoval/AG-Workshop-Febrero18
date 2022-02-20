b82363ce72ac99f5bb7bcddfb92051596b5b5f4d
cristiancsandoval <clinares668@gmail.com>
2022-02-20 09:30:53 -0500

    navbar

--- src/containers/AppTienda.js
+++ src/containers/AppTienda.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 import RouterTienda from '../Router/RouterTienda'
 
 const AppTienda = () => {
   return (
     <div>
+        <Navbar/>
         <RouterTienda/>
     </div>
   )
